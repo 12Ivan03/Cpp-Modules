85c7ebb0ff13a53fd1cdc00a249749d6671e6ef5
Ivan <ivan.pavlov1203@gmail.com>
2025-08-12 11:49:19 +0200

    ex05

--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -16,6 +16,15 @@ int main()
     {
         Weapon club = Weapon("crude spiked club");
         HumanB jim("Jim");
+        jim.setWeapon(club);
+        jim.attack();
+        club.setType("some other type of club");
+        jim.attack();
+    }
+
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanB jim("Josh");
         jim.attack();
         jim.setWeapon(club);
         jim.attack();
