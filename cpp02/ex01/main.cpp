6a7257520f6794e5e0c364e38d334cf7f0b7e83f
Ivan Pavlov <ipavlov@f1r1s2.codam.nl>
2025-08-26 11:45:10 +0200

    ex03

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -6,36 +6,55 @@
 /*   By: ipavlov <ipavlov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 18:14:09 by ipavlov           #+#    #+#             */
-/*   Updated: 2025/08/22 10:25:49 by ipavlov          ###   ########.fr       */
+/*   Updated: 2025/08/26 11:41:34 by ipavlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 int main() {
-
-	Fixed a;
-	Fixed const b( 1032454 );
-	Fixed const c( 42.42345f );
-	Fixed const d( b );
-	Fixed const e( 42.42f );
-	Fixed const f( 10 );
-
-	a = Fixed( 1234.4321f );
-
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "e is " << d << std::endl;
-	std::cout << "f is " << e << std::endl;
-
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	std::cout << "e is " << d.toInt() << " as integer" << std::endl;
-	std::cout << "f is " << e.toInt() << " as integer" << std::endl;
 	
+	{
+		Fixed a;
+		Fixed const b( 10 );
+		Fixed const c( 42.42f );
+		Fixed const d( b );
+		
+		a = Fixed( 1234.4321f );
+		
+		std::cout << "a is " << a << std::endl;
+		std::cout << "b is " << b << std::endl;
+		std::cout << "c is " << c << std::endl;
+		std::cout << "d is " << d << std::endl;
+		std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+		std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+		std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+		std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	}
+	
+	{
+		Fixed a;
+		Fixed const b( 1032454 );
+		Fixed const c( 42.42345f );
+		Fixed const d( b );
+		Fixed const e( 42.42f );
+		Fixed const f( 10 );
+
+		a = Fixed( 1234.4321f );
+
+		std::cout << "a is " << a << std::endl;
+		std::cout << "b is " << b << std::endl;
+		std::cout << "c is " << c << std::endl;
+		std::cout << "d is " << d << std::endl;
+		std::cout << "e is " << d << std::endl;
+		std::cout << "f is " << e << std::endl;
+
+		std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+		std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+		std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+		std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+		std::cout << "e is " << d.toInt() << " as integer" << std::endl;
+		std::cout << "f is " << e.toInt() << " as integer" << std::endl;
+	}
 	return 0;
 }
--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ipavlov <ipavlov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 10:31:23 by ipavlov           #+#    #+#             */
-/*   Updated: 2025/08/25 15:18:33 by ipavlov          ###   ########.fr       */
+/*   Updated: 2025/08/26 11:43:45 by ipavlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,23 +117,28 @@ int main( void ) {
 
 		Fixed aa(3.14159265359f);
 		Fixed bb(2.71f);
-		std::cout << Fixed::max(aa, bb) << std::endl;
+		std::cout << Fixed::max(aa, bb) << std::endl << std::endl;
 		
-		std::cout << std::endl;
+		std::cout << "Copy example from PDF subject" << std::endl << std::endl;
 	
 	}
 
 	// PDF subject example suggestion. 
 	{
-		Fixed a;
-		Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+		
+		Fixed 			a;
+		Fixed const 	b( Fixed( 5.05f ) * Fixed( 2 ) );
+		
 		std::cout << a << std::endl;
 		std::cout << ++a << std::endl;
 		std::cout << a << std::endl;
 		std::cout << a++ << std::endl;
 		std::cout << a << std::endl;
+		
 		std::cout << b << std::endl;
+
 		std::cout << Fixed::max( a, b ) << std::endl;
+		
 		return 0;
 	}
 	
