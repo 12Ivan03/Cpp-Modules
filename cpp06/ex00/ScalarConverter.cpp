bd4fdb74866330204e419e43b9cd02c9a6119ddc
Ivan <ivan.pavlov1203@gmail.com>
2026-02-02 19:05:01 +0200

    ex00

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,6 +1,7 @@
 
 #include "ScalarConverter.hpp"
 #include <algorithm>
+#include <iomanip>
 
 // OCF:
 ScalarConverter::ScalarConverter() {};
@@ -32,7 +33,6 @@ std::string	myTrimString(const std::string &str) {
 
 static void	printChar(double value) {
 
-	// std::cout << "std::isinf(value): " << value << std::endl;
 	std::cout << "char: " ;
 
 	if (std::isinf(value) || std::isnan(value)) {
@@ -42,8 +42,7 @@ static void	printChar(double value) {
 	} else if (value >= 32 && value <= 126) {
 		char c = static_cast<char>(value);
 		std::cout << "\'" << c << "\'" << std::endl;
-	} 
-	else {
+	} else {
 		std::cout << "impossible" << std::endl;
 	} 
 
@@ -56,41 +55,64 @@ static void	printInt(double value) {
 	if (std::isinf(value) || std::isnan(value)) {
 		std::cout << "impossible" << std::endl;
 	} else if (value < INT32_MIN || value > INT32_MAX) {
-		std::cout << "overflow" << std::endl;
+		std::cout << "impossible" << std::endl;
 	} else if (value >= INT32_MIN && value <= INT32_MAX) {
 		std::cout << static_cast<int>(value) << std::endl;
 	} 
 	else {
-		std::cout << "impossible" << std::endl;
+		std::cout << "impossible else" << std::endl;
 	} 
 };
 
-// static void	printFloat(double value) {
+static void	printFloat(double value) {
 
-// };
+	std::cout << "float: ";
+	float output = static_cast<float>(value);
 
-// static void	printDouble(double value) {
+	if  (std::isnan(value)) {
+		std::cout << "nanf" << std::endl;
+	} else if (std::isinf(value) || std::isinf(output)) {
+		std::cout << (output > 0 ? "+inff" : "-inff") << std::endl;
+	} else {
+		std::cout << std::fixed << std::setprecision(1) << output << "f" <<std::endl;
+	}
+};
 
-// };
+static void	printDouble(double value) {
+
+	std::cout << "double: ";
+
+	if  (std::isnan(value)) {
+		std::cout << "nan" << std::endl;
+	} else if (std::isinf(value)) {
+		std::cout << (value > 0 ? "+inf" : "-inf") << std::endl;
+	} else {
+		// std::cout << std::fixed << std::setprecision(1) <<  value << std::endl;
+		std::cout << value << std::endl;
+	}
+};
 
 static void	printAll(double value) {
 
 	printChar(value);
 	printInt(value);
-	// printFloat(value);
-	// printDouble(value);
+	printFloat(value);
+	printDouble(value);
 };
 
 // Error functions
 
 static void errorMessage(int er) {
-	std::cout << "invalue input" << std::endl;
 
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: impossible" << std::endl;
+	std::cout << "double: impossible" << std::endl;
 };
 
-// 1. pseudoLiterals(s)
+// 1. evalPseudoLiterals(s)
 
-static int	pseudoLiterals(const std::string &s) {
+static int	evalPseudoLiterals(const std::string &s) {
 
 	std::string arrLiterals[6] = {"-inff", "+inff", "nanf", "-inf", "+inf", "nan"};
 
@@ -101,12 +123,11 @@ static int	pseudoLiterals(const std::string &s) {
 	return (-1);
 };
 
-static int	evalPseudoLiterals(const std::string &s) {
-	// std::cout << "\tpseudoLiterals" << std::endl;
-	int lit = pseudoLiterals(s);
+static void	printPseudoLiterals(const std::string &s) {
+
+	int lit = evalPseudoLiterals(s);
 	double value;
 	
-	// std::cout << lit << std::endl;
 	if (lit == 2 || lit == 5)
 		value = std::numeric_limits<double>::quiet_NaN();
 	else if (lit == 1 || lit == 4)
@@ -114,8 +135,7 @@ static int	evalPseudoLiterals(const std::string &s) {
 	else
 		value = -std::numeric_limits<double>::infinity();
 	printAll(value);
-	return 0;
-}
+};
 
 // 2. evalStringLength(s)
 
@@ -140,33 +160,90 @@ static void	printStringLength(const std::string &s)
 
 	if (s.length() == 3)
 		index = 1;
-
 	value = static_cast<double>(s[index]);
 	printAll(value);
-}
+};
+
+// 3. evalEndsOnF(s)
+
+static int	evalEndsOnF(const std::string &s) {
+
+	int sLen = s.length() - 1;
+	int dot = 0;
+	char *end = nullptr;
+	double value = strtod(s.c_str(), &end);
+
+	if (end == s.c_str())
+		return (-1);
+	
+	for (int i = sLen - 1; i >= 0; i--) {
+		if (i == 0 && ((s)[i] == '-' || (s)[i] == '+')) {
+			continue ;
+		}
+		if ((s)[i] == '.' && i != 0)
+		{
+			if (!isdigit((s)[i - 1]) || !isdigit((s)[i + 1])) 
+				return (-1);
+			dot++;
+		} else if (!isdigit((s)[i]))
+			return (-1);
+	}
+
+	if (dot == 1)
+		return(1);
+
+	return (-1);
+};
+
+static void	printEndsOnF(const std::string &s) {
+
+	double value = strtod(s.c_str(), nullptr);
+	printAll(value);
+};
+
+// 4. evalStrinIntDouble(s)
+
+static int	evalStrinIntDouble(const std::string &s) {
+
+	int sLen = s.length();
+	// std::cout << "double " << std::endl;
+
+	char *end = nullptr;
+	double value = strtod(s.c_str(), &end);
+	// std::cout << "double value = strtod() ===> " << end[0] << std::endl;
+
+	if (end == s.c_str())
+		return (-1);
+	if (end[0] == '\0')
+		return (1);
+	return (-1);
+};
+
+static void	printStrinIntDouble(const std::string &s) {
+
+	double value = strtod(s.c_str(), nullptr);
+	printAll(value);
+};
 
 // function
 void ScalarConverter::convert(const std::string &str) {
 
-	std::cout << "first entry >" << str << "<" << std::endl;
+	// std::cout << "first entry >" << str << "<" << std::endl;
 
 	std::string s = myTrimString(str);
 	
-	if (pseudoLiterals(s) != -1) {
-		evalPseudoLiterals(s);
-		return ;
+	if (evalPseudoLiterals(s) != -1) {
+		printPseudoLiterals(s);
 	} else if (evalStringLength(s) != -1) {
 		printStringLength(s);
-		return ;
-	// } else if (endsOnF(s)) {
-	// 	printResult();
-	// } else if (strinIntDouble(s)) {
-	// 	printResult();
+	} else if (evalEndsOnF(s) != -1) {
+		printEndsOnF(s);
+	} else if (evalStrinIntDouble(s) != -1) {
+		printStrinIntDouble(s);
 	} else {
-		// printAll(0);
 		errorMessage(1);
 	}
-	std::cout << ">" << s << "<" << std::endl;
+	// std::cout << ">" << s << "<" << std::endl;
 };
 
 // special (nan, inf)
@@ -228,20 +305,4 @@ stages:
 */
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ./convert 12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678989001234567899001123456789012345678902345678901234567890123456789023456789012345678901234567890123456789012345678912345678901234567890123456789123456789012345678901234567809012345678901234567892345678901234567890123456789123456789012345678923456789
