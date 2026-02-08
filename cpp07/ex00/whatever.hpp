ff71c58e21d54a10ca65d4e3adc5058904429ace
Ivan <ivan.pavlov1203@gmail.com>
2026-02-08 05:56:49 +0200

    cpp07

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,27 @@
+
+#include "iostream"
+
+template <typename T>
+void	swap(T& x, T& y) {
+	T temp = x;
+	x = y;
+	y = temp;
+}
+
+template <typename T>
+T& min(T& x, T& y) {
+
+	if (x < y)
+		return x;
+	return y;
+}
+
+template <typename T>
+T& max(T& x, T& y) {
+
+	if (x > y)
+		return x;
+	return y;
+}
+
+// STL - Standard Template Library
