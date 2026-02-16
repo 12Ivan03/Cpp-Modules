840e04da0877f06cf87f8fe97ed1bb42b0ad90de
Ivan <ivan.pavlov1203@gmail.com>
2026-02-16 16:26:26 +0100

    ex02

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -7,12 +7,12 @@ int	main(void) {
 	ms.push(12);
 	ms.push(11);
 	ms.push(6541);
-	std::cout << ms.top() << std::endl;
+	std::cout << "ms = " << ms.top() << std::endl;
 	ms.pop();
-	std::cout << ms.top() << std::endl;
+	std::cout << "ms = " << ms.top() << std::endl;
 
 	MutantStack<int> msNew = ms;
-	std::cout << msNew.top() << std::endl;
+	std::cout << "msNew = " << msNew.top() << std::endl;
 
 	MutantStack<int> msNewNew(msNew);
 	msNewNew.push(123);
@@ -24,5 +24,31 @@ int	main(void) {
 	std::cout << "msNewNew = " << msNewNew.top() << std::endl;
 
 
+	MutantStack<int>::iterator it = msNewNew.begin();
+	std::cout << "it of msNewNew = " << *it << std::endl;
+
+	MutantStack<int>::iterator itEnd = msNewNew.end();
+	--itEnd;
+	std::cout << "itEnd of msNewNew = " << *itEnd << std::endl;
+
+	std::cout << "Loop through ms's values "  << std::endl;
+
+	for (auto it = ms.begin(); it != ms.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	{
+		MutantStack<std::string> mStr;
+		mStr.push("Hello");
+		mStr.push(" ");
+		mStr.push("World");
+		mStr.push("!");
+
+		for (auto it = mStr.begin(); it != mStr.end(); ++it) {
+			std::cout << *it;
+		}
+		std::cout << std::endl;
+	}
 	return (0);
 }
